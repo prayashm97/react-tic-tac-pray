2050eac43420174800e070eaa175e3709410b1bb
Prayash <mprayash@gmail.com>
2017-07-11 21:17:19 -0400

    working on material ui, but first heroku

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,15 @@
 import React,{Component} from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 
 class Home extends Component {
   render(){
     return(
       <div>
+        <RaisedButton
+          onTouchTap={()=>{console.log('hi');}}
+          label="Primary" primary={true}/>
         <h2>Home</h2>
       </div>
     )
--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,17 +1,21 @@
 import React,{Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
-
+injectTapEventPlugin()
 class Template extends Component {
   render(){
     return(
-      <div>
-        <header>
-          <h1>TicTacPray</h1>
-        </header>
-        <main>
-          {this.props.children}
-        </main>
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <header>
+            <h1>TicTacPray</h1>
+          </header>
+          <main>
+            {this.props.children}
+          </main>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
